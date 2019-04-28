dc51d3dde9f0944df91fdfcdf2915a0eab20a37c
wang84947 <849473203@qq.com>
2019-04-28 16:07:49 +0800

    配置自己工程

--- bsp/stm32/stm32f407-atk-explorer/rtconfig.h
+++ bsp/stm32/stm32f407-atk-explorer/rtconfig.h
@@ -39,7 +39,7 @@
 #define RT_USING_CONSOLE
 #define RT_CONSOLEBUF_SIZE 128
 #define RT_CONSOLE_DEVICE_NAME "uart1"
-#define RT_VER_NUM 0x40000
+#define RT_VER_NUM 0x40001
 #define ARCH_ARM
 #define ARCH_ARM_CORTEX_M
 #define ARCH_ARM_CORTEX_M4
@@ -79,6 +79,7 @@
 #define RT_PIPE_BUFSZ 512
 #define RT_USING_SERIAL
 #define RT_SERIAL_USING_DMA
+#define RT_SERIAL_RB_BUFSZ 64
 #define RT_USING_PIN
 
 /* Using WiFi */
@@ -95,6 +96,9 @@
 /* Socket abstraction layer */
 
 
+/* Network interface device */
+
+
 /* light weight TCP/IP stack */
 
 
@@ -108,9 +112,6 @@
 
 
 /* Utilities */
-
-
-/* ARM CMSIS */
 
 
 /* RT-Thread online packages */
@@ -140,15 +141,66 @@
 
 /* tools packages */
 
+#define PKG_USING_SYSTEMVIEW
+#define PKG_SYSVIEW_APP_NAME "RT-Thread Trace"
+#define PKG_SYSVIEW_DEVICE_NAME "Cortex-M"
+#define PKG_SYSVIEW_TIMESTAMP_FREQ 0
+#define PKG_SYSVIEW_CPU_FREQ 0
+#define PKG_SYSVIEW_RAM_BASE 0x20000000
+#define PKG_SYSVIEW_EVENTID_OFFSET 32
+#define PKG_SYSVIEW_USE_CYCCNT_TIMESTAMP
+#define PKG_SYSVIEW_SYSDESC0 "I#15=SysTick"
+#define PKG_SYSVIEW_SYSDESC1 ""
+#define PKG_SYSVIEW_SYSDESC2 ""
+
+/* Segger RTT configuration */
+
+#define PKG_SEGGER_RTT_MAX_NUM_UP_BUFFERS 3
+#define PKG_SEGGER_RTT_MAX_NUM_DOWN_BUFFERS 3
+#define PKG_SEGGER_RTT_BUFFER_SIZE_UP 1024
+#define PKG_SEGGER_RTT_BUFFER_SIZE_DOWN 16
+#define PKG_SEGGER_RTT_PRINTF_BUFFER_SIZE 64
+#define PKG_SEGGER_RTT_AS_SERIAL_DEVICE
+#define PKG_SERIAL_DEVICE_NAME "segger"
+#define PKG_SEGGER_RTT_MODE_ENABLE_NO_BLOCK_SKIP
+#define PKG_SEGGER_RTT_MAX_INTERRUPT_PRIORITY 0x20
+
+/* SystemView buffer configuration */
+
+#define PKG_SEGGER_SYSVIEW_RTT_BUFFER_SIZE 1024
+#define PKG_SEGGER_SYSVIEW_RTT_CHANNEL 1
+#define PKG_SEGGER_SYSVIEW_USE_STATIC_BUFFER
+
+/* SystemView Id configuration */
+
+#define PKG_SEGGER_SYSVIEW_ID_BASE 0x10000000
+#define PKG_SEGGER_SYSVIEW_ID_SHIFT 2
+#define PKG_USING_SYSTEMVIEW_LATEST_VERSION
 
 /* system packages */
 
+#define PKG_USING_GUIENGINE
+#define PKG_USING_GUIENGINE_LATEST_VERSION
+#define PKG_USING_RGB888_PIXEL_BITS_32
+#define PKG_USING_RGB888_PIXEL_BITS 32
+#define GUIENGINE_NAME_MAX 16
+#define GUIENG_USING_FNT_FILE
+#define GUIENGINE_USING_FONT16
+#define GUIENGINE_USING_FONT12
+#define GUIENGINE_USING_PNG
+#define GUIENGINE_IMAGE_LODEPNG
+#define GUIENGINE_IMAGE_CONTAINER
+#define GUIENGINE_USING_DEMO
+#define PKG_USING_PERSIMMON
+#define PKG_USING_PERSIMMON_V100
 
 /* peripheral libraries and drivers */
 
 
 /* miscellaneous packages */
 
+#define PKG_USING_FASTLZ
+#define PKG_USING_FASTLZ_LATEST_VERSION
 
 /* samples: kernel and components samples */
 
